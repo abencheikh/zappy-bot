41ba86fce61e43831f632190c37ed5c41311ca02
Glitch (zappy-bot) <none>
2018-01-12 12:27:04 +0000

    ⛑🏨 Updated with Glitch

--- skills/sample_hears.js
+++ skills/sample_hears.js
@@ -56,6 +56,9 @@ module.exports = function(controller) {
         }
     });
 
+    controller.hears([':zap:'], 'direct_message', function(bot, message) {
+        bot.reply(message, 'saw a :zap:');
+    });
 
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */

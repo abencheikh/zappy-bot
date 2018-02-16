e5e7a557a8c6d38c2cb94799a2a8db77505f3062
Glitch (zappy-bot) <none>
2018-02-16 19:04:36 +0000

    🛫🎐 Updated with Glitch

---
+++ helpers/zaps.js
@@ -0,0 +1,23 @@
+/*
+* Helper function to add zaps
+*/
+
+module.exports = function(giftedUserID, senderID, controller, bot) {
+  if (giftedUserID != senderID) {
+      var nbZap = 0;
+
+      controller.storage.users.get(giftedUserID, function(err, user_data) {
+        if (!err)
+          nbZap = user_data.zaps;
+
+        controller.storage.users.save({id: giftedUserID, zaps: nbZap + 1});
+        
+        if (nbZap == 0) {
+          bot.say( {
+            text: `<@${senderID}> viens de vous envoyer un :zap: ! C'est une manière de vous dire merci pour l'aide, le soutien ou la bonne humeur que vous apportez !\n Vous pouvez faire de même en écrivant :zap: ainsi que @user dans le même message, n'hésitez pas!`,
+            channel: giftedUserID
+          });
+        }
+      });
+  }
+}
--- skills/leaderboard.js
+++ skills/leaderboard.js
@@ -21,12 +21,19 @@ module.exports = function(controller) {
     }); 
   
     controller.hears(['reset'], 'direct_message, direct_mention', function(bot, message) {
-      bot.reply(message, "Resetting leaderboard");
-      
-      controller.storage.users.all(function(err, userList) {
-        userList.map(function(user) {
-         controller.storage.users.delete(user.id); 
-        });
+      bot.api.users.info({user: message.user, token: process.env.verificationToken}, function(err, user) {
+        console.log(user);
+        if (user.is_admin || user.is_owner) {
+          bot.reply(message, "Resetting leaderboard");
+          controller.storage.users.all(function(err, userList) {
+            userList.map(function(user) {
+             controller.storage.users.delete(user.id); 
+            });
+          });
+        }
+        else {
+          bot.reply(message, "Vous n'êtes pas autorisé à effectuer cette commande.");
+        }
       });
     }); 
 }
